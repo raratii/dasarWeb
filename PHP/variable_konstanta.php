1b9c5d0d8f7e94424290eacad39b744168a84696
raratii <purnamaratih419@gmail.com>
2025-09-22 16:18:09 +0700

    ternyata yang tadi salah di falsenya

--- PHP/variable_konstanta.php
+++ PHP/variable_konstanta.php
@@ -1,14 +1,15 @@
 <?php
-$angka1 =10;
-$angka2 =5;
+$angka1 = 10;
+$angka2 = 5;
 $hasil = $angka1 + $angka2;
-echo "hasil penjumlahan $angka1 dan $angka2 adalah $hasil";
+echo "hasil penjumlahan $angka1 dan $angka2 adalah $hasil <br>";
+
 $benar = true;
 $salah = false;
-echo "Variable benar: $benar, Varriable salah: $false";
+echo "Variable benar: $benar, Variable salah: $salah <br>";
 
 define("NAMA_SITUS", "WebsiteKu.com");
 define("TAHUN_PENDIRIAN", 2023);
 
-echo "Selamat Datang di" . NAMA_SITUS . ", situs yang didirikan pada tahun" . TAHUN_PENDIRIAN . ".";
+echo "Selamat Datang di " . NAMA_SITUS . ", situs yang didirikan pada tahun " . TAHUN_PENDIRIAN . ".";
 ?>
