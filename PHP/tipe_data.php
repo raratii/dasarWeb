46832d086cc6f0e90a6694db7fc508d676391a49
raratii <purnamaratih419@gmail.com>
2025-09-22 16:37:56 +0700

    commit3

--- PHP/tipe_data.php
+++ PHP/tipe_data.php
@@ -12,5 +12,16 @@ echo "Variable a: {$d} <br>";
 echo "Variable a: {$e} <br>";
 
 var_dump($e);
-?>
+$nilaiMatematika = 5.1;
+$nilaiIPA= 6.7;
+$nilaiBahasaIndonesia = 9.3;
 
+$rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaIndonesia) /3;
+
+echo "Matematika: {$nilaiMatematika} <br>";
+echo "IPA: {$nilaiIPA} <br>";
+echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
+echo "Rata-rata: {$rataRata} <br>";
+
+var_dump($rataRata);
+?>
