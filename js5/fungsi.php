207192ddb7c66269bf93cd27fdb8c17640b6f67c
raratii <purnamaratih419@gmail.com>
2025-09-28 18:18:46 +0700

    return

--- js5/fungsi.php
+++ js5/fungsi.php
@@ -1,15 +1,7 @@
 <?php
-function perkenalan ($nama, $salam = "Assalamualaikum") {
-    echo $salam.",";
-    echo"Perkenalkan, nama saya " .$nama. "<br/>";
-    echo "Senang berkenalan dengan anda<br/>";
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
 }
-perkenalan("rati","haii");
-
-echo "<hr>";
-
-$saya = "rati";
-$ucapanSalam = "Selamat Pagi!";
-
-perkenalan($saya);
+echo"umur saya adalah " .hitungUmur(2005,2025)."tahun";
 ?>
