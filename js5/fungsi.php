7c506b95bb1a119d506e27f97e0ffe77873f6122
raratii <purnamaratih419@gmail.com>
2025-09-28 18:12:46 +0700

    =

--- js5/fungsi.php
+++ js5/fungsi.php
@@ -1,5 +1,5 @@
 <?php
-function perkenalan ($nama, $salam) {
+function perkenalan ($nama, $salam = "Assalamualaikum") {
     echo $salam.",";
     echo"Perkenalkan, nama saya " .$nama. "<br/>";
     echo "Senang berkenalan dengan anda<br/>";
@@ -11,5 +11,5 @@ echo "<hr>";
 $saya = "rati";
 $ucapanSalam = "Selamat Pagi!";
 
-perkenalan($saya, $ucapanSalam);
+perkenalan($saya);
 ?>
