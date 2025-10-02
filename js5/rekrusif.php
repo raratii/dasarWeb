fcfffe563ccf2387933679552c2a7212427b6496
raratii <purnamaratih419@gmail.com>
2025-10-02 11:58:34 +0700

    nih sblhm di edit

--- js5/rekrusif.php
+++ js5/rekrusif.php
@@ -1,9 +1,43 @@
 <?php
-function tampilkanAngka (int $jumlah, int $indeks =1) {
-     echo"Perulangan ke-{$indeks} <br>";
-     if ($indeks < $jumlah) {
-        tampilkanAngka($jumlah, $indeks +1);
-     }
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama" => "Kuliner"
+            ],
+            [
+                "nama" => "Hiburan"
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ],
+];
+function tampilkanMenuBetingkat(array $menu) {
+    echo"<ul>";
+    foreach ($menu as $key => $item) {
+        echo "<li>{$item['nama']}<li/>";
+    }
+    echo "</ul>";
 }
-tampilkanAngka(20);
+tampilkanMenuBetingkat($menu);
 ?>
